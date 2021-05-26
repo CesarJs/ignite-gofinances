0adb79f40b38f3cc67d52fd44620802878b218cb
Cesar Augusto <cesar.lima@etapa.com.br>
2021-05-26 07:12:02 -0300

    :rocket: Finish C02 AsyncStorage lesson 04

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,11 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Alert, Keyboard, Modal, TouchableWithoutFeedback } from 'react-native';
 
 import * as Yup from 'yup';
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import uuid from 'react-native-uuid';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { useForm } from 'react-hook-form';
+import { useNavigation } from '@react-navigation/native';
 
-import { Input } from '../../components/Form/Input';
 import { InputForm } from '../../components/Form/InputForm';
 import { Button } from '../../components/Form/Button';
 import { TransactionTypeButton } from '../../components/Form/TransactionTypeButton';
@@ -37,6 +39,7 @@ const schema = Yup.object().shape({
 	.required('Você precisa informar o valor!')
 });
 export function Register() {
+	const dataKey = '@gofinacen:transacations';
 	const [ transactionType, setTransacionType] = useState('');
 	const [ showModal, setShowModal ] = useState(false);
 
@@ -44,14 +47,24 @@ export function Register() {
 		key: 'category',
 		name: 'Categoria'
 	});
-
+	const navigation = useNavigation();
 	const {
 		control,
 		handleSubmit,
-		formState: { errors }
+		formState: { errors },
+		reset
 	} = useForm({
 		resolver: yupResolver(schema)
 	});
+
+	function resetForm() {
+		setTransacionType('');
+		setCategory({
+			key: 'category',
+			name: 'Categoria'
+		});
+		reset();
+	}
 	function handleTransactionTypeSelect(type: 'up' | 'down'){
 		setTransacionType(type);
 	}
@@ -62,21 +75,68 @@ export function Register() {
 		setShowModal(false);
 	}
 
-	function handleRegister(form: FormData){
+	async function handleRegister(form: FormData){
 		if(!transactionType){
 			return Alert.alert('Selecione o tipo da transação !');
 		}
 		if(category.key === 'category'){
 			return Alert.alert('Selecione a categoria !');
 		}
-		const data = {
+		const newData = {
+			id: String(uuid.v4()),
 			name: form.name,
 			amount: form.amount,
 			transactionType,
 			category: category.key,
+			date: new Date()
 		}
-		console.log(data)
+		try {
+
+			const data = await AsyncStorage.getItem(dataKey);
+			const currentData = data ? JSON.parse(data) : [];
+			const dataFormatted = [
+
+				...currentData,
+				newData
+			];
+
+			await AsyncStorage.setItem(dataKey, JSON.stringify(dataFormatted));
+
+			resetForm();
+			navigation.navigate("Listagem");
+		} catch (error) {
+			console.log(error);
+			Alert.alert('Não foi possível salvar');
+		}
 	}
+
+	useEffect(() => {
+		// async function loadData(){
+		// 	try {
+		// 		const currentData = await AsyncStorage.getItem(dataKey);
+		// 		console.log(JSON.parse(currentData!));
+		// 	} catch (error) {
+		// 		console.log(error);
+		// 		Alert.alert("Não foi possivel carregar as informações, tente novamente.");
+		// 	}
+
+		// }
+		// loadData();
+
+		// async function deleteData(){
+		// 	const currentData = await AsyncStorage.getItem(dataKey);
+		// 	if(currentData){
+		// 		try {
+		// 			await AsyncStorage.removeItem(dataKey);
+		// 		} catch (error) {
+		// 			console.log(error);
+		// 			Alert.alert('Não foi possivel deletar, por favor tente novamente!');
+		// 		}
+		// 	}
+		// }
+
+		// deleteData();
+	});
 	return (
 		<TouchableWithoutFeedback
 			onPress={Keyboard.dismiss}
